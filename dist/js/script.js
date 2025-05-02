8e117c0b5a4583faa689b4e804cbfd47e810118c
sevycakes <yvescornejo21@gmail.com>
2025-05-02 14:06:16 +0800

    DCCH done

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,59 @@ toggle.addEventListener('click', () => {
     toggle.classList.toggle('open');
     navm.classList.toggle('open');
 });
+
+// SLIDER
+
+const tnslider = tns({
+    container: "#slider_stage",
+    items: 1,
+    loop: true,
+    slideBy: 1,
+    autoplay:true,
+    autoplayButton:false,
+    autoplayButtonOutput:false,
+    speed: 1000,
+    nav:false,
+    controls:false,
+    responsive: {
+        375: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    },
+    swipeAngle: true,
+    speed: 1000
+});
+
+// SPA SLIDER
+
+const tnslider2 = tns({
+    container: "#slider_stage2",
+    items: 1,
+    loop: true,
+    slideBy: 1,
+    autoplay:true,
+    autoplayButton:false,
+    autoplayButtonOutput:false,
+    speed: 1000,
+    nav:false,
+    controls:false,
+    responsive: {
+        375: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    },
+    swipeAngle: true,
+    speed: 1000
+});
